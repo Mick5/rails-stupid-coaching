4b70ae5b5fae547f61832836f481191f8121e04c
Michael Giuliani <michael.pm@libero.it>
2018-04-09 17:51:25 +0200

    stupid c

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  ANSWERS = ["Great!","Silly question, get dressed and go to work!", "I don’t care, get dressed and go to work!" ]
+
+
+  def answer
+    @question = params[:question]
+
+    if @question == "I am going to work"
+     @answer = ANSWERS[0]
+
+   elsif @question[-1] == "?"
+     @answer = ANSWERS[1]
+
+   else  @answer = ANSWERS[2]
+   end
+ end
+end
